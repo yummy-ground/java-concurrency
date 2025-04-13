ec804562824138d94fa4ff8ff12dfcc0a7dffa2e
yummygyudon <bang2brew@gmail.com>
2025-04-13 23:35:36 +0900

    chap 2-1 : Thread & Runnable Creation

---
+++ src/main/java/org/yummyground/chap02/ExtendThread.java
@@ -0,0 +1,23 @@
+package org.yummyground.chap02;
+
+public class ExtendThread {
+    public static void main(String[] args) {
+        Thread worker1 = new WorkerThread();
+        Thread worker2 = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("> Thread Run...");
+            }
+        };
+
+        worker1.start();
+        worker2.start();
+    }
+}
+
+class WorkerThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("> Thread Run...");
+    }
+}
---
+++ src/main/java/org/yummyground/chap02/ImplementRunnable.java
@@ -0,0 +1,28 @@
+package org.yummyground.chap02;
+
+public class ImplementRunnable {
+    public static void main(String[] args) {
+        ExecuteTask task = new ExecuteTask();
+        Thread worker1 = new Thread(task);
+        Thread worker2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Task Execute...");
+            }
+        });
+        Thread worker3 = new Thread(() -> System.out.println("Task Execute..."));
+
+
+        worker1.start();
+        worker2.start();
+        worker3.start();
+    }
+}
+
+class ExecuteTask implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Task Execute...");
+    }
+}
